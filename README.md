03247ba9b89b94790d411232b7cfd671bb2565a8
kisahtegar <code.kisahtegar@gmail.com>
2024-08-07 16:48:33 +0700

    Implementing UI Testing with createComposeAndroidRule.

--- README.md
+++ README.md
@@ -1,8 +1,13 @@
-# Jetpack Compose Labs
+# MyComposeTesting
 
-This repository used for learning and testing:
+## 🔎 Overview
 
-| Project Name                 | Jetpack Compose Labs           |
+MyComposeTesting is a simple application demonstrating the basics of UI testing with Jetpack Compose. 
+The application allows users to calculate the area of a rectangle by entering the length and width.
+
+## 📢 Project Information
+
+| Project Name                 | MyComposeTesting               |
 |------------------------------|--------------------------------|
 | Target & Minimum Target SDK  | Phone and Tablet, API level 24 |
 | Activity Type                | Jetpack Compose                | 
@@ -10,10 +15,17 @@ This repository used for learning and testing:
 | Language                     | Kotlin                         |
 | Build Configuration Language | Kotlin DSL                     |
 
+## 🛠️ Features
+
+- Jetpack Compose UI: Build and design a simple user interface.
+- State Management: Use state variables to manage user input and results.
+- UI Testing: Implement basic UI tests using Jetpack Compose Test.
+
 ## 👀 Previews
 
 This the output application after running the project:
 
 <p align="center">
     <img src="previews/preview_1.png" alt="Preview 1" width="200" height="400">
+    <img src="previews/preview_2.png" alt="Preview 2" width="633" height="223">
 </p>
--- app/src/androidTest/java/com/kisahcode/jetpackcomposelabs/ui/CalculatorAppTest.kt
+++ app/src/androidTest/java/com/kisahcode/jetpackcomposelabs/ui/CalculatorAppTest.kt
@@ -1,18 +1,39 @@
 package com.kisahcode.jetpackcomposelabs.ui
 
-import androidx.compose.ui.test.junit4.createComposeRule
+import androidx.activity.ComponentActivity
+import androidx.compose.ui.test.assertHasNoClickAction
+import androidx.compose.ui.test.junit4.createAndroidComposeRule
 import androidx.compose.ui.test.onNodeWithText
 import androidx.compose.ui.test.performClick
 import androidx.compose.ui.test.performTextInput
+import com.kisahcode.jetpackcomposelabs.R
 import com.kisahcode.jetpackcomposelabs.ui.theme.JetpackComposeLabsTheme
 import org.junit.Before
 import org.junit.Rule
 import org.junit.Test
 
+/**
+ * Test class for the CalculatorApp composable function.
+ *
+ * This class contains UI tests to verify the functionality of the CalculatorApp, including correct
+ * calculations and handling of invalid input.
+ */
 class CalculatorAppTest {
-    @get:Rule
-    val composeTestRule = createComposeRule()
 
+    /**
+     * A JUnit rule to set up the Compose testing environment.
+     *
+     * This rule sets up the activity and initializes the Compose content for testing.
+     */
+    @get:Rule
+    val composeTestRule = createAndroidComposeRule<ComponentActivity>()
+
+    /**
+     * Sets up the content for testing.
+     *
+     * This method is called before each test to set up the CalculatorApp composable within
+     * the JetpackComposeLabsTheme.
+     */
     @Before
     fun setUp() {
         composeTestRule.setContent {
@@ -22,11 +43,50 @@ class CalculatorAppTest {
         }
     }
 
+    /**
+     * Tests the calculation of the area of a rectangle.
+     *
+     * This test inputs valid length and width values, performs the calculation, and verifies
+     * the result displayed in the UI is correct.
+     */
     @Test
     fun calculate_area_of_rectangle_correct() {
-        composeTestRule.onNodeWithText("Masukkan panjang").performTextInput("3")
-        composeTestRule.onNodeWithText("Masukkan lebar").performTextInput("4")
-        composeTestRule.onNodeWithText("Hitung!").performClick()
-        composeTestRule.onNodeWithText("Hasil: 12.0").assertExists()
+        composeTestRule
+            .onNodeWithText(composeTestRule.activity.getString(R.string.enter_length))
+            .performTextInput("3")
+        composeTestRule
+            .onNodeWithText(composeTestRule.activity.getString(R.string.enter_width))
+            .performTextInput("4")
+        composeTestRule
+            .onNodeWithText(composeTestRule.activity.getString(R.string.count))
+            .performClick()
+        composeTestRule
+            .onNodeWithText(composeTestRule.activity.getString(R.string.count), useUnmergedTree = true)
+            .assertHasNoClickAction()
+        composeTestRule
+            .onNodeWithText(composeTestRule.activity.getString(R.string.result, 12.0))
+            .assertExists()
+    }
+
+    /**
+     * Tests the handling of incorrect input.
+     *
+     * This test inputs invalid length values, performs the calculation, and verifies
+     * that the result displayed in the UI is handled correctly (i.e., not calculated).
+     */
+    @Test
+    fun wrong_input_not_calculated() {
+        composeTestRule
+            .onNodeWithText(composeTestRule.activity.getString(R.string.enter_length))
+            .performTextInput("..3")
+        composeTestRule
+            .onNodeWithText(composeTestRule.activity.getString(R.string.enter_width))
+            .performTextInput("4")
+        composeTestRule
+            .onNodeWithText(composeTestRule.activity.getString(R.string.count))
+            .performClick()
+        composeTestRule
+            .onNodeWithText(composeTestRule.activity.getString(R.string.result, 0.0))
+            .assertExists()
     }
 }
